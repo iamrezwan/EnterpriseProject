d43104eaf8a4dfa6f63ad9426a2009950abc27c4
Office <rezwan@live.co.uk>
2018-11-28 00:24:35 +0600

    Enterprise Project Part 1: Starting

---
+++ Example/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Example.Controllers
+{
+    public class MyClass
+    {       
+        public int ID { get; set; }
+        public string MyProperty { get; set; }
+    }
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : Controller
+    {
+        private readonly ILogger _logger;
+        public ValuesController(ILoggerFactory loggerFactory)
+        {            
+            _logger = loggerFactory.CreateLogger<ValuesController>();
+        }
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<JsonResult> Hello(int id = 0)
+        {
+            var x = new MyClass { ID = id, MyProperty = "x" };            
+            return Json(x);
+
+        }
+    }
+}
---
+++ Example/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Example
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(x => x.SwaggerDoc("V1.0", new Info { Title = "Signature", Version = "V1.0" }));
+            services.AddLogging();
+          
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseSwagger(x => x.RouteTemplate = "api/{documentName}/swagger.json");
+            app.UseSwaggerUI(x => {
+                x.SwaggerEndpoint("/api/V1.0/swagger.json", "API V1.0");
+                x.RoutePrefix = string.Empty;
+            });
+            app.UseMvc();
+        }
+    }
+}
